429df8d96297591b97ab859308d4b954323ca0d4
K1DV5 <kidusadugna@gmail.com>
2020-07-12 18:35:38 +0300

    make server more efficient, save input as cache

--- server/server.py
+++ server/server.py
@@ -4,7 +4,7 @@ from http.cookies import BaseCookie
 from glob import glob
 from os import path, remove
 from random import random
-from json import dumps, load, loads
+from json import dumps, load, loads, dump
 from hashlib import pbkdf2_hmac
 from .algo import extract_subjects, makeSchedule
 
@@ -21,7 +21,6 @@ with open(pass_path) as file:
 public_cache = {}
 authenticated = {}
 mime = {'.html': 'text/html', '.css': 'text/css', '.js': 'text/javascript'}
-init = dumps(extract_subjects()).encode()  # for input
 
 
 class handler(BaseHTTPRequestHandler):
@@ -38,20 +37,18 @@ class handler(BaseHTTPRequestHandler):
         # for files
         if self.path == '/':
             self.path = '/index.html'
-        for p in glob('public/**/*', recursive=True):
-            if not path.isfile(p) \
-                    or p.replace('\\', '/')[len('public'):] != self.path:
-                continue
+        fpath = path.join('public', self.path[1:])
+        if path.isfile(fpath) and path.exists(fpath):
             self.send_response(200)
-            self.send_header('Content-Type', mime.get(path.splitext(p)[1], 'text/plain'))
+            self.send_header('Content-Type', mime.get(path.splitext(fpath)[1], 'text/plain'))
             self.end_headers()
-            mtime = path.getmtime(p)
-            if p in public_cache and public_cache[p]['mtime'] == mtime:
-                content = public_cache[p]['content']
+            mtime = path.getmtime(fpath)
+            if fpath in public_cache and public_cache[fpath]['mtime'] == mtime:
+                content = public_cache[fpath]['content']
             else:
-                with open(p, 'rb') as file:
+                with open(fpath, 'rb') as file:
                     content = file.read()
-                    public_cache[p] = {'mtime': mtime, 'content': content}
+                    public_cache[fpath] = {'mtime': mtime, 'content': content}
             self.wfile.write(content)
             return
         self.send_response(404)
@@ -61,92 +58,24 @@ class handler(BaseHTTPRequestHandler):
 
     def do_POST(self):
         if self.path == '/auth':
-            if 'pass' in self.headers:
-                if hash_pass(self.headers['pass']) == password:
-                    token = hash(random())
-                    authenticated[self.client_address[0]] = token
-                    self.send_response(200)
-                    self.send_header('Set-Cookie', f'token={token}; Max-Age={24*3600}')
-                else:
-                    self.send_response(203)
-            elif self.signed_in():
-                self.send_response(200)
-            else:
-                self.send_response(203)
-        elif self.path == '/init':
-            if self.signed_in():
-                self.send_response(200)
-                self.send_header('Access-Control-Allow-Origin', 'http://localhost:5000')  # cors
-                self.send_header('Access-Control-Allow-Credentials', 'true')  # cors
-                self.end_headers()
-                self.wfile.write(init)
-                return
-            else:
-                self.send_response(401)
-        elif self.path == '/make':
-            if self.signed_in():
-                data = loads(self.rfile.read(int(self.headers['Content-Length'])))
-                self.send_response(200)
-                self.send_header('Access-Control-Allow-Origin', 'http://localhost:5000')  # cors
-                self.send_header('Access-Control-Allow-Credentials', 'true')  # cors
-                self.end_headers()
-                sch = makeSchedule(data)
-                self.wfile.write(dumps(sch).encode())
-                return
-            else:
-                self.send_response(401)
-        elif self.path == '/save':
-            if self.signed_in():
-                save_key = self.headers['year'] + '-' + self.headers['semester']
-                length = int(self.headers['Content-Length'])
-                dpath = 'data'
-                if length:  # overwrite
-                    with open(path.join(dpath, save_key + '.json'), 'w') as file:
-                        file.write(self.rfile.read(length).decode())
-                    self.send_response(200)
-                else:  # check if exists
-                    if path.exists(path.join(dpath, save_key + '.json')):
-                        self.send_response(409)
-                    else:
-                        self.send_response(200)
-            else:
-                self.send_response(401)
+            self.auth()
         elif self.path == '/get':
-            if 'year' in self.headers:
-                fpath = path.join('data', self.headers['year'] + '-' + self.headers['semester'] + '.json')
-                if path.exists(fpath):
-                    self.send_response(200)
-                    self.send_header('Access-Control-Allow-Origin', 'http://localhost:5000')  # cors
-                    self.send_header('Access-Control-Allow-Credentials', 'true')  # cors
-                    self.end_headers()
-                    with open(fpath, 'rb') as file:
-                        self.wfile.write(file.read())
-                    return
-                else:
-                    self.send_response(404)
-            self.send_response(200)
-            self.send_header('Access-Control-Allow-Origin', 'http://localhost:5000')  # cors
-            self.send_header('Access-Control-Allow-Credentials', 'true')  # cors
-            self.end_headers()
-            saved = {}
-            for p in glob('data/*.json'):
-                with open(p) as file:
-                    saved[path.splitext(path.basename(p))[0]] = load(file)
-            self.wfile.write(dumps(saved).encode())
-            return
-        elif self.path == '/curriculum':
-            if self.signed_in() or 1:
-                with open('public/subjects.xlsx', 'wb') as file:
-                    file.write(self.rfile.read(int(self.headers['Content-Length'])))
-                extract_subjects()
-                self.send_response(200)
+            self.get()
+        elif self.signed_in():
+            if self.path == '/init':
+                self.init()
+            elif self.path == '/make':
+                self.make()
+            elif self.path == '/save':
+                self.save()
+            elif self.path == '/curriculum':
+                self.curriculum()
             else:
-                self.send_response(401)
+                self.send_response(404)
+                self.end_headers()
         else:
             self.send_response(404)
-        self.send_header('Access-Control-Allow-Origin', 'http://localhost:5000')  # cors
-        self.send_header('Access-Control-Allow-Credentials', 'true')  # cors
-        self.end_headers()
+            self.end_headers()
 
     def do_DELETE(self):
         if self.path == '/auth':  # logout
@@ -161,8 +90,6 @@ class handler(BaseHTTPRequestHandler):
                 self.send_response(200)
             else:
                 self.send_response(404)
-        self.send_header('Access-Control-Allow-Origin', 'http://localhost:5000')  # cors
-        self.send_header('Access-Control-Allow-Credentials', 'true')  # cors
         self.end_headers()
 
     def do_PATCH(self):
@@ -178,7 +105,6 @@ class handler(BaseHTTPRequestHandler):
                 self.send_response(401)
         else:
             self.send_response(404)
-        self.send_header('Access-Control-Allow-Origin', 'http://localhost:5000')  # cors
         self.end_headers()
 
     def do_OPTIONS(self):  # cors
@@ -188,6 +114,85 @@ class handler(BaseHTTPRequestHandler):
         self.send_header('Access-Control-Allow-Headers', 'pass,year,semester,old,new')  # cors
         self.send_header('Access-Control-Allow-Credentials', 'true')  # cors
         self.end_headers()  # cors
+
+    def auth(self):
+        if 'pass' in self.headers:
+            if hash_pass(self.headers['pass']) == password:
+                token = hash(random())
+                authenticated[self.client_address[0]] = token
+                self.send_response(200)
+                self.send_header('Set-Cookie', f'token={token}; Max-Age={24*3600}')
+            else:
+                self.send_response(203)
+        elif self.signed_in():
+            self.send_response(200)
+        else:
+            self.send_response(203)
+        self.end_headers()
+
+    def init(self):
+        self.send_response(200)
+        self.end_headers()
+        init_data = extract_subjects()  # for input
+        input_cache_file = path.join('data', 'input-cache.json')
+        if path.exists(input_cache_file):
+            with open(input_cache_file) as file:
+                init_data['input_cache'] = load(file)
+        self.wfile.write(dumps(init_data).encode())
+
+    def make(self):
+        data = loads(self.rfile.read(int(self.headers['Content-Length'])))
+        self.send_response(200)
+        self.end_headers()
+        sch = makeSchedule(data)
+        self.wfile.write(dumps(sch).encode())
+        # save input cache
+        input_cache_file = path.join('data', 'input-cache.json')
+        with open(input_cache_file, 'w') as file:
+            dump(data, file)
+
+    def save(self):
+        save_key = self.headers['year'] + '-' + self.headers['semester']
+        length = int(self.headers['Content-Length'])
+        dpath = 'data'
+        if length:  # overwrite
+            with open(path.join(dpath, save_key + '.json'), 'w') as file:
+                file.write(self.rfile.read(length).decode())
+            self.send_response(200)
+        else:  # check if exists
+            if path.exists(path.join(dpath, save_key + '.json')):
+                self.send_response(409)
+            else:
+                self.send_response(200)
+        self.end_headers()
+
+    def curriculum(self):
+        with open('public/subjects.xlsx', 'wb') as file:
+            file.write(self.rfile.read(int(self.headers['Content-Length'])))
+        extract_subjects()
+        self.send_response(200)
+        self.end_headers()
+
+    def get(self):
+        if 'year' in self.headers:  # specific semester
+            fpath = path.join('data', self.headers['year'] + '-' + self.headers['semester'] + '.json')
+            if path.exists(fpath):
+                self.send_response(200)
+                self.end_headers()
+                with open(fpath, 'rb') as file:
+                    self.wfile.write(file.read())
+            else:
+                self.send_response(404)
+                self.end_headers()
+            return
+        # a list
+        self.send_response(200)
+        self.end_headers()
+        saved = {}
+        for p in glob('data/*.json'):
+            with open(p) as file:
+                saved[path.splitext(path.basename(p))[0]] = load(file)
+        self.wfile.write(dumps(saved).encode())
 
 
 # :%s,^.*cors\n,,g to remove cors lines
